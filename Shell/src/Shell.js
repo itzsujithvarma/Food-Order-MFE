f0a695d7e004bd8a7a5efdaadd903c1972a978b8
Sujith Varma EPURI <sujith.epuri@hexagon.com>
2023-06-09 10:12:40 +0530

    state changes

--- Shell/src/Shell.js
+++ Shell/src/Shell.js
@@ -6,13 +6,14 @@ import Header from 'header/HeaderIndex';
 const Shell = () => {
 const [totalPrice, setTotalPrice] = useState(0);
 const [items, setItems] = useState([]);
+const updateStates = (det)=>{
+    setItems(det.items);
+    setTotalPrice(det.totalPrice);
+}
 return(
         <React.Fragment>
             <Header items = {items}/>
-            <Meals ReceiveItems = {(passedDetails) => {
-                setItems(passedDetails.items);
-                setTotalPrice(passedDetails.totalPrice);
-            }}/>
+            <Meals ReceiveItems = {updateStates}/>
         </React.Fragment>
     );
 }
