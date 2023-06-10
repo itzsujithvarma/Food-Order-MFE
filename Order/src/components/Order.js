57e29a01b76a6b23b75d41c5094ba6ac31fa9dcf
Sujith Varma EPURI <sujith.epuri@hexagon.com>
2023-06-10 19:56:57 +0530

    Order page created

---
+++ Order/src/components/Order.js
@@ -0,0 +1,18 @@
+import React from "react";
+import classes from './Order.module.css';
+import Modal from './UI/Modal';
+
+const Order = (props) => {
+
+return (
+    <Modal onCloseCart = {props.onHideCart}>
+        <div className={classes.total}>
+            <span>Congratulation! Your Order is successful.</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']} onClick = {props.onHideCart}>Close</button>
+        </div>
+    </Modal>
+);
+}
+export default Order;
