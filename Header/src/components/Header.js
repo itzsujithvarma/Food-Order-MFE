f3a1f6668a4d7281f767ec78a8d7110a39ec8580
Sujith Varma EPURI <sujith.epuri@hexagon.com>
2023-06-11 23:13:56 +0530

    Changed header text

--- Header/src/components/Header.js
+++ Header/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return (
         <Fragment>
             <header className={classes.header}>
-                <h1>React Meals</h1>
+                <h1>Food Order MFE</h1>
                 <HeaderCartButton items={props.items} onOpenCart = {props.showCart}/>
             </header>
             <div className={classes['main-image']}>
